d3999f58c2041e047aa31c85c03ffea1c39b6c86
Ebuka Obukwelu <obukweluebuka@gmail.com>
2022-10-27 18:48:20 +0200

    include global css

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,9 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/** Global css */
+import "./global.css"
+
 setupIonicReact();
 
 const App: React.FC = () => (
